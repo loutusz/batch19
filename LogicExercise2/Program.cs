68b003a1702e06577dd9c94b09a1ba1fbca18324
loutusz <j1008.id@gmail.com>
2026-06-08 10:33:22 +0700

    LogicExercise2

---
+++ LogicExercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    string temp = "";
+    if (i % 3 == 0) temp += "foo";
+    if (i % 5 == 0) temp += "bar";
+    if (i % 7 == 0) temp += "jazz";
+
+    Console.Write(temp == "" ? i : temp);
+
+    if (i != number) Console.Write(", ");
+}
+
